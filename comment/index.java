02273dfdf66117d1c2343a044defeee987916e17
ajjiad <jslworld365@gmail.com>
2023-07-05 19:43:36 +0600

    add newfile: comment

---
+++ comment/index.java
@@ -0,0 +1,8 @@
+package comment;
+
+public class index {
+    public static void main(String[] args){
+        System.out.println("hey, Jiad"); //this is the comment
+    }
+}
+/* this is 2nd choice for comment */
