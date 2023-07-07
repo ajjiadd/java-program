8178f964c989bcb3e8cc0b495834eee78add18a9
ajjiad <jslworld365@gmail.com>
2023-07-07 18:19:33 +0600

    add newfile: java_math/boolean_math.java

---
+++ java_math/boolean_math.java
@@ -0,0 +1,9 @@
+package java_math;
+
+public class boolean_math {
+    public static void main(String[] args){
+        int x = 25;
+        int y = 30;
+        System.out.println("Your results is: " + ( x > y));   //Output will be: false
+    }
+}
