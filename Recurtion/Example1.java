cdaa6801e40cf4f0d374ed1c8c2e82a5450fe8c1
ajjiad <jslworld365@gmail.com>
2023-07-09 18:33:43 +0600

    modified:   Recurtion/Example1.java

--- Recurtion/Example1.java
+++ Recurtion/Example1.java
@@ -4,13 +4,19 @@ public class Example1 {
 
     public static void main(String[] args){
         //1st function call;
-        System.out.println(sum(10));
+        int xxx = sum(10);
+        System.out.println(xxx);
+
+        //2nd function call
+        int matt2 = mat2(5, 10);
+        System.out.println(matt2);
 
     }
 
     //1st function;
 
     public static int sum(int x1){
+        
             if(x1 > 0){
                 return x1 + sum(x1 - 1);
             }else{
@@ -18,7 +24,14 @@ public class Example1 {
             }
     }
 
-    
+    //2nd function
+    public static int mat2(int start, int end){
+        if(end > start){
+            return end + mat2(start, end - 1);
+        }else{
+            return end;
+        }
+    }
 
 
 
